70b1f1d8fb98721564354898f453161850a1b037
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-25 17:22:20 -0700

    started weather app

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+
+
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     $(".city").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+//   });
+// }
+
+
+// // var autoLocation = "api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
+
+
+
+// // $.getJSON("api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon)
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    autoLocation = "http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
+    console.log(autoLocation)
+    $.getJSON(autoLocation, function(json) {
+      $(".city").html("<p>latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude +"</p>");
+    })
+  });
+}
+
+
+
+})
