d5b089a60acf88881ad4228fd0bc323aaf363d15
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-25 21:51:15 -0700

    added background, got user location, temp from API

--- javascript.js
+++ javascript.js
@@ -1,41 +1,56 @@
+// // get temp from API
+
+// var changeCity = function() {
+//   $(".city").html("<p>latitude: " + lat + "<br>longitude: " + lon +"</p>")
+// }
 
 
 
 
 // if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     $(".city").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-//   });
+//   navigator.geolocation.getCurrentPosition(successPosition);
+// }
+
+// function successPosition(position) {
+
+//     lat = position.coords.latitude;
+//     lon = position.coords.longitude;
+//     autoLocation = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
+
+//     $.getJSON(autoLocation, changeCity)
 // }
 
 
-// // var autoLocation = "api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
+
+$.ajax({
+  url: "https://geoip-db.com/jsonp",
+  jsonpCallback: "callback",
+  dataType: "jsonp",
+  success: function( location ) {
 
 
+    //change city name to user city name
+    $('.city').html(location.city + ", " + location.state);
 
-// // $.getJSON("api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon)
+    lat = location.latitude;
+    lon = location.longitude;
 
-
-
-
-
-
-
-$(document).ready(function() {
+    var weatherAPI = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
 
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    autoLocation = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
-    console.log(autoLocation)
-    $.getJSON(autoLocation, function(json) {
-      $(".city").html("<p>latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude +"</p>");
+    //change weather to user city weather
+    $.getJSON(weatherAPI, function(json) {
+
+      var temp = Math.round(json.main.temp)
+      var tempMin = json.main.temp_min
+      var tempMax = json.main.temp_max
+      var weatherDes = json.weather[0].main
+
+      $(".temp").html(temp + " " + String.fromCharCode(176) + "F")
+      $(".sky").html(weatherDes)
     })
-  });
-}
+  }
+});
 
 
-
-})
+//https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=37.5963&lon=-122.065
