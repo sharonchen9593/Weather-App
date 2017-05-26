b4b785e9703aad58a4307ac1c128755868603e18
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-25 17:28:17 -0700

    made weather api an https instead of http

--- javascript.js
+++ javascript.js
@@ -28,7 +28,7 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-    autoLocation = "http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
+    autoLocation = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?APPID=68b320a63d26b299f8f806732a6d3b5e&units=imperial&lat=" + lat +"&lon="+ lon
     console.log(autoLocation)
     $.getJSON(autoLocation, function(json) {
       $(".city").html("<p>latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude +"</p>");
